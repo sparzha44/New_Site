46a730eea5d9c3b5a82595a77930b48070f67d69
sparzha44 <henryrus.ilya@gmail.com>
2025-06-06 10:42:08 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,31 @@ document.getElementById('aboutLink').addEventListener('click', function(event) {
 
 UnnomineGigs — это место, где рождаются новые таланты, собираются любители музыки и создается атмосфера настоящего живого звука. Мы гордимся своей работой и стремимся делать каждую встречу с музыкой особенной!`);
 });
+
+  // Скрипт для увеличения и уменьшения возраста
+  const ageInput = document.getElementById('age');
+  document.getElementById('increase').addEventListener('click', () => {
+    let current = parseInt(ageInput.value);
+    if (current < parseInt(ageInput.max)) {
+      ageInput.value = current + 1;
+    }
+  });
+  document.getElementById('decrease').addEventListener('click', () => {
+    let current = parseInt(ageInput.value);
+    if (current > parseInt(ageInput.min)) {
+      ageInput.value = current - 1;
+    }
+  });
+
+  // Обработка отправки формы
+  document.getElementById('purchaseForm').addEventListener('submit', function(event) {
+    const age = parseInt(ageInput.value);
+    if (age < 16) {
+      alert('Вход запрещен для лиц младше 16 лет.');
+      event.preventDefault(); // отменить отправку формы
+    } else if (age < 18) {
+      alert('Вход разрешен только в сопровождении взрослых.');
+      event.preventDefault(); // отменить отправку формы
+    }
+    // иначе форма отправляется
+  });
