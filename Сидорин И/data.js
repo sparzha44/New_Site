055aa2eb0eef71d276fc4709f9b2cb54997b4f4c
sparzha44 <henryrus.ilya@gmail.com>
2025-10-13 13:37:28 +0300

    Add files via upload

---
+++ Сидорин И/data.js
@@ -0,0 +1,39 @@
+const dataInfo = [{
+        "id": "ef6",
+        "url": "https://i.pinimg.com/736x/00/42/2f/00422fd3f56b711066ce657f7e122def.jpg",
+        "width": 500,
+        "height": 334
+    },
+    {
+        "id": "bpl",
+        "url": "https://i.pinimg.com/736x/ac/f1/98/acf198aa1812360838ffab7e336b1b1c.jpg",
+        "width": 500,
+        "height": 1061
+    },
+    {
+        "id": "d7p",
+        "url": "https://avatars.mds.yandex.net/i?id=9023613d4eae69b7fbacd640eace7147_sr-10705627-images-thumbs&n=13",
+        "width": 765,
+        "height": 1024
+    }
+];
+
+const data = [{
+        "id": "ef6",
+        "url": "https://i.pinimg.com/736x/00/42/2f/00422fd3f56b711066ce657f7e122def.jpg",
+        "width": 500,
+        "height": 334
+    },
+    {
+        "id": "bpl",
+        "url": "https://i.pinimg.com/736x/ac/f1/98/acf198aa1812360838ffab7e336b1b1c.jpg",
+        "width": 500,
+        "height": 1061
+    },
+    {
+        "id": "d7p",
+        "url": "https://avatars.mds.yandex.net/i?id=9023613d4eae69b7fbacd640eace7147_sr-10705627-images-thumbs&n=13",
+        "width": 765,
+        "height": 1024
+    }
+];
